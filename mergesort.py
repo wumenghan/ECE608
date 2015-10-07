fd9bed29e19478079afc77c843235a48ba79cb57
wumenghan <wu784@purdue.edu>
2015-10-07 17:53:44 -0400

    add merge

--- mergesort.py
+++ mergesort.py
@@ -1,28 +1,27 @@
 from __future__ import unicode_literals
 import random,time
-def mergesort(list):
+def mergesort(list,empty_list):
 	n = len(list)
 	if ( n < 2 ):
 		return
 	half_size = n/2
 	left_list = list[:half_size]
 	right_list = list[half_size:]
-	mergesort(left_list)
-	mergesort(right_list)
-	Merge(left_list,right_list,list)
+	mergesort(left_list,empty_list)
+	mergesort(right_list,empty_list)
+	Merge(left_list,right_list,empty_list)
 
 def Merge(left_list,right_list,list):
-	for left in left_list:
-		for right in right_list:
-			if left < right:
-				list.append(left)
-			else:
-				list.append(right)
-
+	left_index = len(left_list) -1
+	right_index = len(right_list) -1
+	left_start = 0
+	right_start = 0
+	while left_start < left_inde
 def main():
-	list = [1,2,3,5,2,34,5]
-	mergesort(list)
-	print list
+	list = [1,2,3,5]
+	empty_list = []
+	mergesort(list,empty_list)
+	print empty_list
 
 if __name__ == "__main__":
 	main()
